b61bcfc9db95265192c8d2ac78dba070d29f3299
essenius <essenius@users.noreply.github.com>
2024-01-07 14:34:37 +0100

    3.1.1: fixed static analysis findings and cleanup

--- GameManagement/GameManagementFixtures/Overview.cs
+++ GameManagement/GameManagementFixtures/Overview.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2013-2021 Rik Essenius
+﻿// Copyright 2013-2024 Rik Essenius
 //
 //   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file 
 //   except in compliance with the License. You may obtain a copy of the License at
@@ -18,7 +18,7 @@ namespace GameManagementFixtures
 
     public class Overview
     {
-        public static TableList DoTable(TableList table) => new TableList
+        public static TableList DoTable(TableList table) => new()
         {
             new List<string>{ "report:Number of players",
                 "pass:" + StaticGame.Players.Count }
--- GameManagement/GameManagementFixtures/StaticGame.cs
+++ GameManagement/GameManagementFixtures/StaticGame.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2013-2021 Rik Essenius
+﻿// Copyright 2013-2024 Rik Essenius
 //
 //   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file 
 //   except in compliance with the License. You may obtain a copy of the License at
@@ -15,7 +15,6 @@ namespace GameManagementFixtures
 {
     public abstract class StaticGame
     {
-        public static PlayerCollection Players { get; } = new PlayerCollection();
+        public static PlayerCollection Players { get; } = new();
     }
 }
-
